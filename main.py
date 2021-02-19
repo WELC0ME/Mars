3f6ed482214d80d708f42407e551561fcda1e119
WELC0ME <zz-crazy-zz@mail.ru>
2021-02-19 13:40:00 +0300

    task 03

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    return '</br>'.join([
+        'Человечество вырастает из детства.',
+        'Человечеству мала одна планета.',
+        'Мы сделаем обитаемыми безжизненные пока планеты.',
+        'И начнем с Марса!',
+        'Присоединяйся!'
+    ])
+
+
+@app.route('/image_mars')
+def image_mars():
+    return open('static/html/image_mars.txt', 'r', encoding='utf8').read()
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')

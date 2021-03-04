a541acc23d5052402b263845757a8de84ea4976e
WELC0ME <zz-crazy-zz@mail.ru>
2021-03-04 21:06:33 +0300

    task_01

--- main.py
+++ main.py
@@ -1,86 +1,14 @@
-from flask import Flask, request
-from PIL import Image
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    return "Миссия Колонизация Марса"
-
-
-@app.route('/index')
-def index():
-    return "И на Марсе будут яблони цвести!"
-
-
-@app.route('/promotion')
-def promotion():
-    return '</br>'.join([
-        'Человечество вырастает из детства.',
-        'Человечеству мала одна планета.',
-        'Мы сделаем обитаемыми безжизненные пока планеты.',
-        'И начнем с Марса!',
-        'Присоединяйся!'
-    ])
-
-
-@app.route('/image_mars')
-def image_mars():
-    return open('static/html/image_mars.txt', 'r', encoding='utf8').read()
-
-
-@app.route('/promotion_image')
-def promotion_image():
-    return open('static/html/promotion_image.txt', 'r', encoding='utf8').read()
-
-
-@app.route('/astronaut_selection', methods=['POST', 'GET'])
-def astronaut_selection():
-    if request.method == 'GET':
-        return open('static/html/astronaut_selection.txt', 'r',
-                    encoding='utf8').read()
-    elif request.method == 'POST':
-        print(request.form.get('surname'))
-        print(request.form.get('name'))
-        print(request.form.get('email'))
-        print(request.form.get('education'))
-        print(request.form.getlist('profession'))
-        print(request.form.get('sex'))
-        print(request.form.get('about'))
-        print(request.files['file'].read().decode('utf-8'))
-        print(request.form.get('accept'))
-        return "Форма отправлена"
-
-
-@app.route('/choice/<planet_name>')
-def choice(planet_name):
-    return open('static/html/choice.txt', 'r',
-                encoding='utf8').read().format(planet_name)
-
-
-@app.route('/results/<nickname>/<int:level>/<float:rating>')
-def results(nickname, level, rating):
-    return open('static/html/results.txt', 'r',
-                encoding='utf8').read().format(nickname, level, rating)
-
-
-@app.route('/load_photo', methods=['POST', 'GET'])
-def load_photo():
-    if request.method == 'POST':
-        try:
-            Image.open(request.files['file']).save('static/temp/loaded.png')
-            res = 'Image loaded'
-        except Exception as e:
-            res = 'Error: ' + str(e)
-        return res
-    elif request.method == 'GET':
-        return open('static/html/load_photo.txt', 'r', encoding='utf8').read()
-
-
-@app.route('/carousel')
-def carousel():
-    return open('static/html/carousel.txt', 'r', encoding='utf8').read()
+@app.route('/index/<title>')
+def index(title):
+    params = {
+        'title': title,
+    }
+    return render_template('base.html', **params)
 
 
 if __name__ == '__main__':

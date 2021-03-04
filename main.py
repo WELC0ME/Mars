7434e613eddef8dbc6a41a4205c84b65961eb539
WELC0ME <zz-crazy-zz@mail.ru>
2021-03-04 21:28:38 +0300

    task_02

--- main.py
+++ main.py
@@ -11,5 +11,24 @@ def index(title):
     return render_template('base.html', **params)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    if any([i in prof for i in ['инженер', 'строитель']]):
+        heading = 'Инженерные тренажеры'
+        image = 'engineer.jpg'
+    elif any([i in prof for i in ['ученый', 'программист']]):
+        heading = 'Научные симуляторы'
+        image = 'scientist.jpg'
+    else:
+        heading = 'Неизвестно'
+        image = 'question.jpg'
+    params = {
+        'title': 'Mars',
+        'heading': heading,
+        'image': image,
+    }
+    return render_template('training.html', **params)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

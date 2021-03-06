d1a3a41244ebbdf3259a52020debfeae75b057da
WELC0ME <zz-crazy-zz@mail.ru>
2021-03-06 18:50:56 +0300

    task_04

--- main.py
+++ main.py
@@ -69,5 +69,21 @@ def distribution():
     return render_template('distribution.html', **params)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    params = {
+        'title': 'Mars',
+        'surname': 'Watny',
+        'name': 'Mark',
+        'education': 'выше среднего',
+        'profession': 'штурман марсохода',
+        'sex': 'male',
+        'motivation': 'Всегда мечтал застрять на Марсе!',
+        'ready': 'True',
+    }
+    return render_template('auto_answer.html', **params)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

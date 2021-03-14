31b32f4b754eef0e3e58cf18a3defa1ca0f52de1
WELC0ME <zz-crazy-zz@mail.ru>
2021-03-14 17:34:33 +0300

    task_13

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from PIL import Image
 import os
-from loginform import LoginForm
+from forms.loginform import LoginForm
+from forms.registerform import RegisterForm
 from data import db_session
 from data.jobs import Jobs
+from data.users import User
 
 
 app = Flask(__name__)
@@ -144,6 +146,35 @@ def member():
 def journal():
     db_sess = db_session.create_session()
     return render_template("journal.html", jobs=db_sess.query(Jobs).all())
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
